cb842d622ab2c011211eb29da84aba124227d50a
Le Mong Thuy <thuyle2000@gmail.com>
2022-09-05 17:26:39 +0700

    Upload Sep 05, 2022

---
+++ d06_array_strstr.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// chuong trinh luu tru danh sach ho ten  cua n-sinh vien 
+// va tim sinh vien theo ten duoc yeu cau
+int main()
+{
+    system("cls");
+
+    int n;
+    while (1==1)
+    {
+        printf(" nhap so luong sinh vien [3-10]: ");
+        scanf("%d", &n);
+        if(n>=3 && n<=10){
+            break;
+        }
+    }
+
+    fflush(stdin);
+
+    char tensv[n][31]; //khai bao mang chuoi (string) gom n-phantu, moi chuoi co the chua toi da 30 ky tu (char)
+
+    //vong lap nhap ten cua n-sinh vien
+    for (int i = 0; i < n; i++)
+    {
+        printf(" >> ten ho ten cua sv thu %d: ", i+1);
+        gets(tensv[i]);
+    }
+    
+    //vong lap in ra bang ds ho ten cua n-sinh vien
+    printf("\n Danh sach sinh vien \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf(" %2d. %s \n", i+1, tensv[i]);
+    }
+
+    //khai bao bien chuoi chua ten cua sv muon tim trong danh sach
+    char s[31];
+    printf("\n nhap ten cua sv muon tim: ");
+    gets(s);
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if(strstr(tensv[i], s)){
+            printf(" %d. %s \n", count+1, tensv[i]);
+            count++;
+        }
+    } //ket thuc vong lap FOR
+
+    if(count==0){
+        printf(" >> KO Tim Thay !!!");
+    }
+
+    
+}
